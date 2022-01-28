c9cac91c0a525f2464c7b5b6db9b08790dc157b0
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2022-01-27 19:43:23 -0500

    create vote model, link to posts

--- app/models/Post.py
+++ app/models/Post.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from sqlite3 import Date
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 
 class Post(Base):
@@ -17,3 +18,7 @@ class Post(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
