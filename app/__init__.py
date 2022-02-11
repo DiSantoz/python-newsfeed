a2d44e428cb5d6f3d1bb2ffc0348166639068167
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2022-02-10 19:58:20 -0500

    render login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from mimetypes import init
 from app.routes import home
-from app.routes import dashboard
+from app.routes import home, dashboard, api
 from flask import Flask
 from app.db import init_db
 from app.utils import filters
@@ -21,6 +21,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
     # filers
     app.jinja_env.filters['format_url'] = filters.format_url
     app.jinja_env.filters['format_date'] = filters.format_date
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,3 +3,5 @@ from .home import bp as home
 
 # import bp object from dashboard file and rename to dashboard
 from .dashboard import bp as dashboard
+
+from .api import bp as api
